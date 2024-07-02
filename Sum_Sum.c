93672bf1f1bb35d2a0cb9ed22fc7cf4137354aa9
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-07-02 23:16:56 +0600

    Add files via upload

---
+++ Sum_Sum.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main() {
+    int N,positive=0,negative=0;
+    scanf("%d",&N);
+    int a[N]; 
+    for (int i = 0; i<N; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    
+    for (int i = 0; i<N; i++) {
+        if(a[i]>0)
+        {
+            positive += a[i]; 
+        }else{
+            negative += a[i];
+        }
+    }
+    
+    printf("%d %d\n",positive,negative);
+    return 0;
+
+
+}
