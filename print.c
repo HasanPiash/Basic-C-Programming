1e253f32d84ce4fa9729f53f5eb7992b185a493c
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2024-07-04 20:24:18 +0600

    Print

--- print.c
+++ print.c
@@ -1,6 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    printf("Hello");
+    // printf("Hello\tHi I am a programmer");
+
+    printf("100%%%%");
     return 0;
 }
+
+// Hello
+// Hi I am a programmer
+
+// Special characters (ESCAPE)
+// \n - new line
+// \t - tab
