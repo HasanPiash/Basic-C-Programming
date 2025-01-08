94611620531aba177243e5fcb34959346e79d9f5
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-01-08 23:17:19 +0600

    Create conditional

---
+++ 007_conditional_1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100)
+    {
+        printf("Buy Phone");
+    }
+    else
+    {
+        printf("Limitations");
+    }
+    return 0;
+}
