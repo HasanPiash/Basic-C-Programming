0f32045666a63d32ff54ebadd3173ccddeb22302
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-01-09 22:24:22 +0600

    Create conditional

---
+++ 009_conditional_3.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=5000)
+    {
+        printf("Cox's Bazar Tour\n");
+        if(tk>=10000)
+        {
+            printf("Saint Martin Tour\n");
+        }
+        else
+        {
+            printf("Go back\n");
+        }
+    }
+    else
+    {
+        printf("No Tour\n");
+    }
+    return 0;
+}
