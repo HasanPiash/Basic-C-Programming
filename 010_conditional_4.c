ee68636293dc02624ca58a44d290aaec2a300ec0
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-01-09 22:26:08 +0600

    Create conditional

---
+++ 010_conditional_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int tk;
+    scanf("%d", &tk);
+    if (tk>=1000)
+    {
+        printf("Buy pizza\n");
+        printf("Then Dayout\n");
+    }
+
+    return 0;
+}
