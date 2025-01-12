964b7bb18246ade1a431a4c85e0b8b0af7931bf9
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-01-13 00:52:55 +0600

    Create Conditional Statement

---
+++ 024_Conditional_Statement.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char a;
+    scanf("%c",&a);
+    if(a>='a' && a<='z')
+    {
+        // Lowercase
+        int ans=a-32;
+        printf("%c",ans);
+    }
+    else 
+    {
+        int ans=a+32;
+        printf("%c",ans);
+    }
+    return 0;
+}
