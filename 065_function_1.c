62a97f0d032711eb60d8062e9a37533ea6058652
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2025-02-10 21:47:16 +0600

    Create Function 1

---
+++ 065_function_1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int sum(int x,int y) // x=20 y=25
+{
+    int sum=x+y;
+    return sum;
+}
+int main()
+{
+    int x,y;
+    scanf("%d %d",&x,&y);
+    printf("%d",sum(x,y));
+    return 0;
+}
