0da67590fdff0f7970449d8c7db11a05bcf3ff13
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-02-26 20:45:09 +0600

    Update Count_Me2.c

--- Count_Me2.c
+++ Count_Me2.c
@@ -14,6 +14,7 @@ int main() {
     int len = strlen(S);
   
     // Count consonants in the string
+    
     for (int i = 0; i < len; i++) {
         if (!is_vowel(S[i])) {
             consonants++;
