86370b4026b5fb7db819081f333428dab785269b
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-02-27 14:39:13 +0600

    Update Reverse_and_Odd.c

--- Reverse_and_Odd.c
+++ Reverse_and_Odd.c
@@ -4,6 +4,7 @@ int main()
     int N;
     scanf("%d",&N);
     int a[N]; 
+    
     for (int i = 0; i<N; i++){
         scanf("%d",&a[i]);
     }
@@ -14,4 +15,5 @@ int main()
         }
     }
     return 0;
+
 }
