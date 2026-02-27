80ca9db34e73e7555aea7fe19699195d6ccf7bf5
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-02-27 14:39:54 +0600

    Update Sum_Sum.c

--- Sum_Sum.c
+++ Sum_Sum.c
@@ -3,6 +3,7 @@ int main() {
     int N,positive=0,negative=0;
     scanf("%d",&N);
     int a[N]; 
+    
     for (int i = 0; i<N; i++)
     {
         scanf("%d", &a[i]);
@@ -19,6 +20,5 @@ int main() {
     
     printf("%d %d\n",positive,negative);
     return 0;
-
 
 }
