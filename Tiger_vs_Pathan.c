b887502bf78d43688f67faebf92c7d7bd0339525
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-02-28 11:44:13 +0600

    Update Tiger_vs_Pathan.c

--- Tiger_vs_Pathan.c
+++ Tiger_vs_Pathan.c
@@ -5,12 +5,14 @@ int main() {
     scanf("%d", &T); // Number of test cases
 
     for (int t = 1; t <= T; t++) { // Loop through test cases from 1 to T
+        
         int N;
         scanf("%d", &N); // Number of rounds
         char S[N+1];
         scanf("%s", S); // String representing rounds
         
         int tiger_wins = 0, pathan_wins = 0;
+        
         for (int i = 0; i < N; i++) {
             if (S[i] == 'T')
                 tiger_wins++;
@@ -28,3 +30,4 @@ int main() {
 
     return 0;
 }
+
