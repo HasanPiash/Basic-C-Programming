4ac95ad02e0321239df8ce2d0fb8b0bc2b449e73
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-11 14:46:34 +0600

    Update magical_tree.c

--- magical_tree.c
+++ magical_tree.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 int main() {
     int n;
     scanf("%d", &n);
-
     int m = 6 + (n / 2);
 
     // Printing upper part of the tree
@@ -15,7 +13,6 @@ int main() {
         for (int j = 1; j <= k; j++) {
             printf("*");
         }
-        
         printf("\n");
     }
 
@@ -31,6 +28,5 @@ int main() {
         
         printf("\n");
     }
-
     return 0;
 }
