84009ea1172f694b291b012e83effe0c111fb6dc
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-03-11 14:47:55 +0600

    Update odd_and_Even.c

--- odd_and_Even.c
+++ odd_and_Even.c
@@ -3,6 +3,7 @@ void odd_even(){
     int n;
     scanf("%d",&n);
     int even=0, odd=0;
+    
     for (int i=0; i<n;i++)
     {
         int num;
@@ -18,7 +19,9 @@ void odd_even(){
     }
     printf("%d %d\n",even,odd);
 }
+
 int main(){
     odd_even();
     return 0;
+
 }
