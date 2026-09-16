3cee00e7a56007787210e74d63dc78458a397587
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-16 20:21:19 +0600

    Assignment : arithmetic

--- arithmetic.c
+++ arithmetic.c
@@ -2,8 +2,7 @@
 int main()
 {
     int a = 3, b = 20;
-    int mod = a % b;
-    
+    int mod = a % b;  
     printf("%d", mod);
     return 0;
 }
@@ -11,6 +10,6 @@ int main()
 // a + b
 // a - b
 // a * b
-
 // a / b -> 5/2 - > int ans = a*1.0/2
 // a % b
+ 
