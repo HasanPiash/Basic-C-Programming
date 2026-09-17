b0b8e270232a3ead9d2db674d349246a70e11152
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-17 18:47:12 +0600

    Assignment : conditional 4

--- conditional_4.c
+++ conditional_4.c
@@ -3,12 +3,11 @@ int main()
 {
     int tk;
     scanf("%d", &tk);
-    if (tk >= 1000)
+    if (tk >= 1000) 
     {
-        printf("Ami pizza khabo\n");
-        printf("Ami ghurte jabo\n");
+        printf("Ami pizza khabo\n"); 
+        printf("Ami ghurte jabo\n"); 
     }
-
     return 0;
 
 }
