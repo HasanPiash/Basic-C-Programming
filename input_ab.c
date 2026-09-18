5fa16ccd5e2406685c0391d679f757d113fbbaea
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-18 19:23:18 +0600

    Assignment : input ab

--- input_ab.c
+++ input_ab.c
@@ -1,10 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int apple,banana;
-    char p;
-    
+    int apple,banana; 
+    char p; 
     scanf("%d%c %d%c",&apple, &p, &banana, &p);
     printf("%da %da", apple, banana);
-    return 0;
+    return 0; 
 }
