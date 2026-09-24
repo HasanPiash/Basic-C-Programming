ca4db9c96840eea3e84ce370e09afe24d149d22c
Hasan Piash <83497033+HasanPiash@users.noreply.github.com>
2026-09-24 17:33:28 +0000

    Assignment : limitation  int

--- limitation_int.c
+++ limitation_int.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int a=1000000000;
-    printf("%d",a);
+    int a=1000000000; 
+    printf("%d",a); 
     return 0;
 }
-
-
 // max value limitation 10. otherwise print garbage value
